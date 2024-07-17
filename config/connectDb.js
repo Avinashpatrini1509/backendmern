240ca79124ff136b43dd1a480e63c18ec66aec7c
Avinashpatrini1509 <avinashpatrini@gmail.com>
2024-07-17 19:31:32 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/financial");
+    await mongoose.connect("mongodb+srv://avinash:avinash123@cluster0.acuw1hu.mongodb.net/");
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
